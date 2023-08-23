b11b13d3a627c0f49bb34699757290204da2e0c8
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-23 22:38:02 +0200

    fixing price field to a correct type in a constructor

--- BillsBurgers/src/Hamburger.java
+++ BillsBurgers/src/Hamburger.java
@@ -12,7 +12,7 @@ public class Hamburger {
     private  String addition4Name;
     private  double addition4Price;
 
-    public Hamburger(String name, String meat, int price, String breadRollType) {
+    public Hamburger(String name, String meat, double price, String breadRollType) {
         this.name = name;
         this.meat = meat;
         this.price = price;
