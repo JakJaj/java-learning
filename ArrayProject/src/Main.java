5d32c3c890d5fdc7a4843abd3b4e4d696a036544
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-24 21:50:28 +0200

    tried using some array's methodes

--- ArrayProject/src/Main.java
+++ ArrayProject/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        int[] fistArray = randomArray(10);
+        printArray(fistArray);
+
+        int[] secondArray = Arrays.copyOf(fistArray, fistArray.length);
+
+        printArray(secondArray);
+
+        secondArray[5] = 11;
+        secondArray[0] = 123;
+
+        System.out.println("Zmiana");
+        printArray(fistArray);
+        printArray(secondArray);
+
+    }
+
+    public static int[] randomArray(int lenght){
+
+        int[] array = new int[lenght];
+        Random random = new Random();
+
+        for(int i = 0; i < lenght; i++){
+            array[i] = random.nextInt(100);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        System.out.println(Arrays.toString(array));
     }
 }
