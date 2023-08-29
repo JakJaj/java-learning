bca0929f2220c72601fb2701666820bd444d39e9
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-29 22:27:01 +0200

    implement method that reverse a array

--- ReverseArrayChallange/src/Main.java
+++ ReverseArrayChallange/src/Main.java
@@ -2,4 +2,9 @@
 public class Main {
     public static void main(String[] args) {
     }
+    ReverseArray ra = new ReverseArray();
+
+    int[] array = {1,2,3,4,5,6};
+
+
 }
--- ReverseArrayChallange/src/ReverseArray.java
+++ ReverseArrayChallange/src/ReverseArray.java
@@ -1,2 +1,13 @@
+import java.util.Arrays;
+
 public class ReverseArray {
+    private static void reverse(int[] array){
+        int[] reversed = new int[array.length];
+        System.out.println(Arrays.toString(array));
+        for(int i = 0; i < array.length; i ++){
+            reversed[reversed.length - i - 1] = array[i];
+        }
+        array = reversed;
+        System.out.println(Arrays.toString(array));
+    }
 }
