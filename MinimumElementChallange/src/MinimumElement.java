ebd16173c7120cb776686dc11beb9acdf6002b50
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-29 21:42:51 +0200

    add methode that gets an int from user

--- MinimumElementChallange/src/MinimumElement.java
+++ MinimumElementChallange/src/MinimumElement.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class MinimumElement {
+
+    public static int readInteger(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pass an integer:");
+        return scanner.nextInt();
+    }
+    
 }
