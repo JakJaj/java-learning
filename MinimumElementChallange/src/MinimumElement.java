f965e7463bce17859f3d7961997a5e48f0f099e6
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-29 21:48:36 +0200

    add method that create array based on user input

--- MinimumElementChallange/src/MinimumElement.java
+++ MinimumElementChallange/src/MinimumElement.java
@@ -7,5 +7,14 @@ public class MinimumElement {
         System.out.println("Pass an integer:");
         return scanner.nextInt();
     }
+    public static int[] readElements(int length){
+        int[] array = new int[length];
+        System.out.println("Created array that can store " + length + " integers");
+        for(int i = 0; i < length; i++){
+            System.out.println("Integer #" + i);
+            array[i] = readInteger();
+        }
+        return array;
+    }
     
 }
