33e4e23b9237f4a84ee9ee7cc0cf84965f6c896f
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-08-31 22:49:10 +0200

    add check that adding works

--- MobilePhoneChallange/src/Main.java
+++ MobilePhoneChallange/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        MobilePhone mobilePhone = new MobilePhone("123123123");
+        Contact adam = new Contact("Adam","321321321");
 
+        mobilePhone.printContacts();
+        mobilePhone.addNewContact(adam);
+        mobilePhone.printContacts();
+        System.out.println(mobilePhone.addNewContact(adam));
     }
 }
