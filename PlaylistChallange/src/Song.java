8ca27753249e2df9b163409d2a4324d35bbefa2f
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-09-03 21:09:33 +0200

    create Song class

--- PlaylistChallange/src/Song.java
+++ PlaylistChallange/src/Song.java
@@ -1,2 +1,21 @@
 public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        if(duration < 0){
+            this.duration = 0;
+        }
+        else this.duration = duration;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title + ": " + duration;
+    }
 }
