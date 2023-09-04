4cadcf7a573a7ebcfef0a0a6ed1492901ccca301
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-09-04 19:26:45 +0200

    make Customer field final

--- BankingChallange/src/Customer.java
+++ BankingChallange/src/Customer.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Customer {
-    private String name;
-    private ArrayList <Double> transactions;
+    private final String name;
+    private final ArrayList <Double> transactions;
 
     public Customer(String name, double initialTransaction) {
         this.name = name;
