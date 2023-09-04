46deb280f78df66dbe3f4f3c6c6a32dba66da3fb
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-09-04 19:46:53 +0200

    fill in Branch class

--- BankingChallange/src/Branch.java
+++ BankingChallange/src/Branch.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Branch {
+    private final String name;
+    private final ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public Customer findCustomer(String customerName){
+        for(Customer element: customers){
+            if(element.getName().equals(customerName)) return element;
+        }
+        return null;
+    }
+    public boolean newCustomer(String name, double initialTransaction){
+        if(findCustomer(name) == null){
+            customers.add(new Customer(name,initialTransaction));
+            return true;
+        }
+        return false;
+    }
+    public boolean addCustomerTransaction(String name, double transaction){
+        Customer foundCustomer = findCustomer(name);
+        if(foundCustomer == null){
+            return false;
+        }
+        foundCustomer.addTransaction(transaction);
+        return true;
+    }
 }
