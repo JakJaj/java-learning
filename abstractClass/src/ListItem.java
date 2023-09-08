875b6752557a6709840356c0e6efb19f0a15f130
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-09-08 20:04:33 +0200

    create ListItem class

--- abstractClass/src/ListItem.java
+++ abstractClass/src/ListItem.java
@@ -1,2 +1,19 @@
-public class ListItem {
+public abstract class ListItem {
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+    public ListItem(Object value){
+        this.value = value;
+    }
+    abstract ListItem next();
+    abstract ListItem setNext();
+    abstract ListItem previous();
+    abstract ListItem setPrevious();
+    abstract int compareTo();
+    public Object getValue(){
+        return value;
+    }
+    public void setValue(Object value){
+        this.value = value;
+    }
 }
